7c55620ff45f22dd304f271e240309beae88a743
Timer91 <marais.jeremie@gmail.com>
2016-05-19 04:52:52 +0200

    Contact PHP script + Ajax call + revisions on form

---
+++ js/validate.js
@@ -0,0 +1,64 @@
+$( function() {
+	$( '#form' ).validate({
+		rules: {
+			name: {
+				required: true,
+				minlength: 2
+			},
+			email: {
+				required: true,
+				email: true
+			},
+			message: {
+				required: true
+			},
+            surname: {
+                required: true
+            },
+            phone: {
+                required: true
+            },
+            city: {
+                required: true
+            }
+		},
+		messages: {
+			name: {
+				required: "Veuillez entrer un nom",
+				minlength: "Votre nom doit contenir au moins 2 caractères"
+			},
+			email: {
+				required: "Un email est requis"
+			},
+			message: {
+				required: "Veuillez écrire un message",
+			},
+            surname: {
+                required: "Veuillez entrer un nom"
+            },
+            phone: {
+                required: "Veuillez entrer un numéro de téléphone"
+            },
+            city: {
+                required: "Veuillez entrer votre ville"
+            }
+		},
+		submitHandler: function( form ) {
+            $.post( "php/contact.php", $( form ).serialize(), function() {
+                $( '#form :input' ).attr( 'disabled', 'disabled' );
+                $( '#form' ).fadeTo( "slow", 0.15, function() {
+                    $( this ).find( ':input' ).attr( 'disabled', 'disabled' );
+                    $( this ).find( 'label' ).css( 'cursor','default' );
+                    $( '#success' ).fadeIn();
+                });
+            })
+            .done( function() {
+            })
+            .fail( function() {
+                $('#form').fadeTo( "slow", 0.15, function() {
+                    $('#error').fadeIn();
+                });
+            })
+        }
+	});
+});
---
+++ php/contact.php
@@ -0,0 +1,16 @@
+<?php
+    $to = "thierry.marais@amserv-it.fr";
+    $from = $_REQUEST['email'];
+    $name = $_REQUEST['name'];
+    $headers = "De: $from";
+    $subject = "Vous avez reçu un mail de votre site web";
+    $fields = array();
+    $fields{"name"} = "Prénom";
+    $fields{"surname"} = "Nom";
+    $fields{"email"} = "Email";
+    $fields{"phone"} = "Téléphone";
+    $fields{"city"} = "Ville";
+    $fields{"message"} = "Message";
+    $body = "Voici le message:\n\n"; foreach($fields as $a => $b){ $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
+    $send = mail($to, $subject, $body, $headers);
+?>
